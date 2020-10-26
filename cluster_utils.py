456723f050a070eba82d05c5ab672c6e393aad81
anderzzz <ohrn.anders@gmail.com>
2020-10-26 16:00:05 +0100

    Now handle the case of ragged numpy array properly, using boolean masks and the compress method

--- cluster_utils.py
+++ cluster_utils.py
@@ -14,8 +14,6 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import normalize
 from scipy.spatial.distance import cosine as cosine_distance
 
-from functools import reduce
-
 class VectorUpdateError(Exception):
     pass
 
@@ -43,8 +41,9 @@ class MemoryBank(object):
     def __init__(self, n_vectors, dim_vector, memory_mixing_rate=None):
 
         self.dim_vector = dim_vector
-        self.memory_vectors = np.array([marsaglia(dim_vector) for _ in range(n_vectors)])
+        self.vectors = np.array([marsaglia(dim_vector) for _ in range(n_vectors)])
         self.memory_mixing_rate = memory_mixing_rate
+        self.mask_init = np.array([False] * n_vectors)
 
     def update_memory(self, vectors, index, memory_mixing_rate=None):
         '''Update the memory with new vectors
@@ -56,17 +55,37 @@ class MemoryBank(object):
             self.memory_mixing_rate = memory_mixing_rate
 
         if isinstance(index, int):
-            self.memory_vectors[index] = self._update_(vectors, self.memory_vectors[index])
+            self.vectors[index] = self._update_(vectors, self.vectors[index])
 
         elif isinstance(index, np.ndarray):
             for ind, vector in zip(index, vectors):
-                self.memory_vectors[ind] = self._update_(vector, self.memory_vectors[ind])
+                self.vectors[ind] = self._update_(vector, self.vectors[ind])
 
         else:
             raise RuntimeError('Index must be of type integer or NumPy array, not {}'.format(type(index)))
 
-    def __getitem__(self, idx):
-        return torch.tensor(self.memory_vectors[idx], requires_grad=False)
+    def mask(self, inds_int):
+        '''Construct a Boolean mask given integer indices.
+
+        The integer indices can be of different lengths, which complicate some operations. By converting the list
+        of integers into several boolean vectors, the lengths are the same.
+
+        Args:
+            inds_int (numpy array): A nested array of arrays, each element an integer corresponding to a memory
+                vector. The nested array can be comprised of arrays of different size, a so-called ragged array.
+
+        Returns:
+            inds_bool (numpy array): A matrix, each element a boolean defining if corresponding memory vector
+                should be selected. This array can be used to select or compress the set of memory vectors.
+
+        '''
+        ret_mask = []
+        for row in inds_int:
+            row_mask = np.full(self.vectors.shape[0], False)
+            row_mask[row.astype(int)] = True
+            ret_mask.append(row_mask)
+
+        return np.array(ret_mask)
 
     def _update_(self, vector_new, vector_recall):
         return vector_new * self.memory_mixing_rate + vector_recall * (1.0 - self.memory_mixing_rate)
@@ -89,12 +108,13 @@ class LocalAggregationLoss(nn.Module):
                  k_nearest_neighbours, clustering_repeats, number_of_centroids,
                  memory_bank,
                  kmeans_n_init=1, nn_metric=cosine_distance, nn_metric_params={},
-                 include_self_index=True):
+                 include_self_index=True, force_stacking=False):
         super(LocalAggregationLoss, self).__init__()
 
         self.temperature = temperature
         self.memory_bank = memory_bank
         self.include_self_index = include_self_index
+        self.force_stacking = force_stacking
 
         self.background_neighbours = None
         self.close_neighbours = None
@@ -107,27 +127,27 @@ class LocalAggregationLoss(nn.Module):
             self.clusterer.append(KMeans(n_clusters=number_of_centroids,
                                          init='random', n_init=kmeans_n_init))
 
-    def _nearest_neighbours(self, codes, indices):
+    def _nearest_neighbours(self, codes_data, indices):
         '''Ascertain indices in memory bank of the k-nearest neighbours to given codes
 
         Args:
-            codes:
+            codes_data:
             indices:
 
         Returns:
-            indices_nearest (numpy.ndarray): Indices of k-nearest neighbours for the batch of codes
+            indices_nearest (numpy.ndarray): Boolean array of k-nearest neighbours for the batch of codes
 
         '''
-        self.neighbour_finder.fit(self.memory_bank.memory_vectors)
-        indices_nearest = self.neighbour_finder.kneighbors(codes, return_distance=False)
+        self.neighbour_finder.fit(self.memory_bank.vectors)
+        indices_nearest = self.neighbour_finder.kneighbors(codes_data, return_distance=False)
 
-        if self.include_self_index:
+        if not self.include_self_index:
             self_neighbour_masks = [np.where(indices_nearest[k] == indices[k]) for k in range(indices_nearest.shape[0])]
             if any([len(x) != 1 for x in self_neighbour_masks]):
                 raise RuntimeError('Self neighbours not correctly shaped')
             indices_nearest = np.delete(indices_nearest, self_neighbour_masks, axis=1)
 
-        return indices_nearest
+        return self.memory_bank.mask(indices_nearest)
 
     def _close_grouper(self, indices):
         '''Ascertain indices in memory bank of vectors that are in the same cluster as vectors of given indices
@@ -136,45 +156,71 @@ class LocalAggregationLoss(nn.Module):
             indices (numpy.ndarray):
 
         Returns:
-            indices_close (numpy.ndarray):
+            indices_close (numpy.ndarray): Boolean array of close neighbours for the batch of codes
 
         '''
         memberships = [[]] * len(indices)
         for clusterer in self.clusterer:
-            clusterer.fit(self.memory_bank.memory_vectors)
+            clusterer.fit(self.memory_bank.vectors)
             for k_index, cluster_index in enumerate(clusterer.labels_[indices]):
                 other_members = np.where(clusterer.labels_ == cluster_index)[0]
                 other_members_union = np.union1d(memberships[k_index], other_members)
                 memberships[k_index] = other_members_union.astype(int)
 
-        return np.array(memberships)
+        return self.memory_bank.mask(np.array(memberships, dtype=object))
 
     def _intersecter(self, n1, n2):
-        '''Compute set intersection of two arrays of indices
+        '''Compute set intersection of two boolean arrays
+
+        Args:
+            n1 (numpy array): Boolean array specifying a first selection of memory vectors
+            n2 (numpy array): Boolean array specifying a second selection of memory vectors
+
+        Returns:
+            n1n2 (numpy array): Boolean array specifying the intersected selection of memory vectors of inputs
 
         '''
-        ret = [np.intersect1d(n1_x, n2_x) for n1_x, n2_x in zip(n1, n2)]
+        ret = [[v1 and v2 for v1, v2 in zip(n1_x, n2_x)] for n1_x, n2_x in zip(n1, n2)]
         return np.array(ret)
 
-    def _prob_density(self, codes, indices):
-        '''Bla bla
+    def _prob_density(self, codes, indices, force_stack=False):
+        '''Compute the unnormalized probability density for the codes being in the sets defined by the indices
+
+        The routine contains two ways to compute the densities, one where the batch dimension is handled using
+        PyTorch function `bmm`, and one where the batch dimension is explicitly iterated over. The values obtained
+        do not differ, but performance might. The former method is only possible if the subsets of data points are
+        of identical size in the batch. If that is not true (the array is "ragged"), the iteration plus stacking
+        is the only option.
+
+        Args:
+            codes
+            indices
+            force_stack (bool, optional): Even if the subsets are identical in size in the batch, compute densities
+                with the iterate and stack method
+
+        Returns:
+            prob_dens (Tensor): The unnormalized probability density of the vectors with given codes being part
+                of the subset of codes specified by the indices. There is one dimension, the batch dimension
 
         '''
-        # In case the indices are all of the same dimension, broadcasting can be used and the
-        # batch dimension is handled concisely.
-        print (codes)
-        print (indices)
-        if len(set([len(x) for x in indices])) == 1:
-            vals = self.memory_bank[indices]
+        ragged = len(set([np.count_nonzero(ind) for ind in indices])) != 1
+
+        # In case the subsets of memory vectors are all of the same size, broadcasting can be used and the
+        # batch dimension is handled concisely. This will always be true for the k-nearest neighbour density
+        if not ragged and not force_stack:
+            vals = torch.tensor([np.compress(ind, self.memory_bank.vectors, axis=0) for ind in indices],
+                                requires_grad=False)
             v_dots = torch.bmm(vals, codes.unsqueeze(-1))
             exp_values = torch.exp(torch.div(v_dots, self.temperature))
             xx = torch.sum(exp_values, dim=1).squeeze(-1)
 
-        # When broadcasting not possible, manually loop over batch dimension and stack results
+        # Broadcasting not possible if the subsets of memory vectors are of different size, so then manually loop
+        # over the batch dimension and stack results
         else:
             xx_container = []
             for k_item in range(codes.size(0)):
-                vals = self.memory_bank[indices[k_item]]
+                vals = torch.tensor(np.compress(indices[k_item], self.memory_bank.vectors, axis=0),
+                                    requires_grad=False)
                 v_dots_prime = torch.mv(vals, codes[k_item])
                 exp_values_prime = torch.exp(torch.div(v_dots_prime, self.temperature))
                 xx_prime = torch.sum(exp_values_prime, dim=0)
@@ -184,43 +230,49 @@ class LocalAggregationLoss(nn.Module):
         return xx
 
     def forward(self, codes, indices):
-        '''Bla bla
+        '''Forward pass for the local aggregation loss module
+
+        Args:
+            codes:
+            indices:
+
+        Returns:
+            loss:
 
         '''
         assert codes.shape[0] == len(indices)
 
+        code_data = normalize(codes.detach().numpy(), axis=1)
+
         # Compute and collect arrays of indices that define the constants in the loss function. Note that
         # no gradients are computed for these data values in backward pass
-        code_data = normalize(codes.detach().numpy(), axis=1)
         self.memory_bank.update_memory(code_data, indices)
         self.background_neighbours = self._nearest_neighbours(code_data, indices)
         self.close_neighbours = self._close_grouper(indices)
         self.neighbour_intersect = self._intersecter(self.background_neighbours, self.close_neighbours)
 
-        print ('Q1',self.background_neighbours)
-        print ('Q2',self.close_neighbours)
-        print ('Q3',self.neighbour_intersect)
-
         # Compute the probability density for the codes given the constants of the memory bank
         v = F.normalize(codes, p=2, dim=1)
-        d1 = self._prob_density(v, self.background_neighbours)
-        d2 = self._prob_density(v, self.neighbour_intersect)
-        loss_cluster = torch.log(d1) - torch.log(d2)
+        d1 = self._prob_density(v, self.background_neighbours, self.force_stacking)
+        d2 = self._prob_density(v, self.neighbour_intersect, self.force_stacking)
+        loss_cluster = torch.sum(torch.log(d1) - torch.log(d2)) / codes.shape[0]
 
         return loss_cluster
 
 
 def test_la():
     np.random.seed(42)
-    mbank = MemoryBank(7,3,1.0)
-    laloss = LocalAggregationLoss(temperature=1.0, k_nearest_neighbours=5,
-                                  memory_bank=mbank, clustering_repeats=2, number_of_centroids=2)
+    mbank = MemoryBank(8,3,1.0)
+    laloss = LocalAggregationLoss(temperature=2.0, k_nearest_neighbours=5,
+                                  memory_bank=mbank, clustering_repeats=2, number_of_centroids=3)
     vvv = torch.tensor([[1.0, 0.0, -0.2],
-                        [0.0, -1.0, 4.0]], requires_grad=True, dtype=torch.float64)
-    laloss.forward(vvv, np.array([0,2]))
-    raise RuntimeError
+                        [0.0, -1.0, 4.0],
+                        [0.5,2.0,0.0]], requires_grad=True, dtype=torch.float64)
+    loss = laloss(vvv, np.array([0,2,3]))
+    print (loss)
 
 test_la()
+
 
 class ClusterHardnessLoss(nn.Module):
     '''Cluster Hardness Loss function as described in equations 4-6 in 'Clustering with Deep Learning: Taxonomy
@@ -296,7 +348,6 @@ class ClusterHardnessLoss(nn.Module):
 
 def test1():
 
-    import numpy as np
     from torch import autograd
 
     # Compute module for dummy input
